95698dfef9c5652c4cc0377441c001829ab18390
unknown <abaron@regionsjob.dom>
2022-12-22 20:40:31 +0100

    start

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -5,122 +5,123 @@ using Genius;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace app.Controllers
 {
-	public class HomeController : Controller
-	{
-		public readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        public readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-		[Route("/", Name = "Home")]
-		public IActionResult Index(string nameArtist)
-		{
-			var rappeurs = DataService.Artists;
-			var pager = new Pagination(rappeurs.Count, 1, 10, 11);
-			if (!string.IsNullOrEmpty(nameArtist))
-				rappeurs = DataService.Artists.Where(r => r.Name.ToLowerInvariant().Contains(nameArtist.ToLowerInvariant())).OrderByDescending(r => r.Popularity).ToList();
-			else
-				rappeurs = rappeurs.OrderByDescending(r => r.Popularity).Take(pager.PageSize).ToList();
+        [Route("/", Name = "Home")]
+        public IActionResult Index(string nameArtist)
+        {
+            var rappeurs = DataService.Artists;
+            var pager = new Pagination(rappeurs.Count, 1, 10, 11);
+            if (!string.IsNullOrEmpty(nameArtist))
+                rappeurs = DataService.Artists.Where(r => r.Name.ToLowerInvariant().Contains(nameArtist.ToLowerInvariant())).OrderByDescending(r => r.Popularity).ToList();
+            else
+                rappeurs = rappeurs.OrderByDescending(r => r.Popularity).Take(pager.PageSize).ToList();
 
-			var model = new HomeViewModel()
-			{
-				Artists = rappeurs,
-				Pagination = pager,
-				ArtistsCount = DataService.Artists.Count
+            var model = new HomeViewModel()
+            {
+                Artists = rappeurs,
+                Pagination = pager,
+                ArtistsCount = DataService.Artists.Count
             };
 
-			return View(model);
-		}
+            return View(model);
+        }
 
-		[Route("{number:int}", Name = "Pagination")]
-		public IActionResult Pagination(int number)
-		{
-			var currentPage = number;
-			var pager = new Pagination(DataService.Artists.Count, currentPage, 20, 11);
-			var rappeurs = DataService.Artists.OrderByDescending(r => r.Popularity).Skip((currentPage - 1) * pager.PageSize).Take(pager.PageSize).ToList();
+        [Route("{number:int}", Name = "Pagination")]
+        public IActionResult Pagination(int number)
+        {
+            var currentPage = number;
+            var pager = new Pagination(DataService.Artists.Count, currentPage, 20, 11);
+            var rappeurs = DataService.Artists.OrderByDescending(r => r.Popularity).Skip((currentPage - 1) * pager.PageSize).Take(pager.PageSize).ToList();
 
-			var model = new HomeViewModel()
-			{
-				Artists = rappeurs,
-				Pagination = pager
-			};
+            var model = new HomeViewModel()
+            {
+                Artists = rappeurs,
+                Pagination = pager
+            };
 
-			return View("Index", model);
-		}
+            return View("Index", model);
+        }
 
-		[Route("artist_{idArtist}/album_{idAlbum}", Name = "Album")]
-		public IActionResult Album(string idArtist, string idAlbum)
-		{
-			var album = DataService.Albums.FirstOrDefault(a => a.Id == idAlbum);
-			if (album == null)
-				return NotFound();
-			album.Artist = DataService.Artists.FirstOrDefault(a => a.Id == idArtist);
+        [Route("artist_{idArtist}/album_{idAlbum}", Name = "Album")]
+        public IActionResult Album(string idArtist, string idAlbum)
+        {
+            var album = DataService.Albums.FirstOrDefault(a => a.Id == idAlbum);
+            if (album == null)
+                return NotFound();
+            album.Artist = DataService.Artists.FirstOrDefault(a => a.Id == idArtist);
+            var albumSimple = album.Artist.Albums.Items.FirstOrDefault(a => a.Id == idAlbum);
+            album.Name = albumSimple.Name;
 
-			return View(album);
-		}
+            return View(album);
+        }
 
-		[Route("artist_{id}", Name = "Artist")]
-		public IActionResult Artist(string id)
-		{
-			var artist = DataService.Artists.FirstOrDefault(a => a.Id == id);
-			if (artist == null)
-				return NotFound();
+        [Route("artist_{id}", Name = "Artist")]
+        public IActionResult Artist(string id)
+        {
+            var artist = DataService.Artists.FirstOrDefault(a => a.Id == id);
+            if (artist == null)
+                return NotFound();
 
-			return View(artist);
-		}
+            return View(artist);
+        }
 
-		[Route("/labels", Name = "Labels")]
-		public IActionResult Labels()
-		{
-			return View(DataService.Labels);
-		}
+        [Route("/labels", Name = "Labels")]
+        public IActionResult Labels()
+        {
+            return View(DataService.Labels);
+        }
 
 
-		[Route("artist_{idArtist}/album_{idAlbum}/track_{idTrack}", Name = "Track")]
-		public IActionResult Track(string idArtist, string idAlbum, string idTrack)
-		{
-			var artist = DataService.Artists.FirstOrDefault(a => a.Id == idArtist);
-			if (artist == null)
-				return NotFound();
-			var albumSimple = artist.Albums.Items.FirstOrDefault(a => a.Id == idAlbum);
-			var album = DataService.Albums.FirstOrDefault(a => a.Id == idAlbum);
-			if (album == null)
-				return NotFound();
-			var track = album.Tracks.FirstOrDefault(t => t.Id == idTrack);
-			if (track == null)
-				return NotFound();
+        [Route("artist_{idArtist}/album_{idAlbum}/track_{idTrack}", Name = "Track")]
+        public IActionResult Track(string idArtist, string idAlbum, string idTrack)
+        {
+            var artist = DataService.Artists.FirstOrDefault(a => a.Id == idArtist);
+            if (artist == null)
+                return NotFound();
+            var albumSimple = artist.Albums.Items.FirstOrDefault(a => a.Id == idAlbum);
+            var album = DataService.Albums.FirstOrDefault(a => a.Id == idAlbum);
+            if (album == null)
+                return NotFound();
+            var track = album.Tracks.FirstOrDefault(t => t.Id == idTrack);
+            if (track == null)
+                return NotFound();
 
-			track.Album = album;
-			track.Album.Name = albumSimple.Name;
-			track.Album.Artist = artist;
+            track.Album = album;
+            track.Album.Name = albumSimple.Name;
+            track.Album.Artist = artist;
 
-			var genius = new GeniusClient("lK00bVSgu4CJpmnp8Db8Lo2K0mYrrjBM5Z66sc6PWbZUP1nkkC08YABXd_JFyh0c");
-			var songsfind = genius.SearchClient.Search(track.Name + " by " + artist.Name).Result;
-			var songfindUrl = songsfind.Response.Hits.FirstOrDefault();
-			if (songfindUrl != null)
-			{
-				HtmlWeb web = new HtmlWeb();
-				HtmlDocument doc = web.Load(songfindUrl.Result.Url);
-				var headerNames = doc.DocumentNode.SelectNodes("//*[@id='lyrics-root']/div[2]");
-				track.Text = System.Web.HttpUtility.HtmlDecode(headerNames.FirstOrDefault().InnerText);
-				if (track.Text == null)
-					return NotFound();
-			};
+            var genius = new GeniusClient("lK00bVSgu4CJpmnp8Db8Lo2K0mYrrjBM5Z66sc6PWbZUP1nkkC08YABXd_JFyh0c");
+            var songsfind = genius.SearchClient.Search(track.Name + " by " + artist.Name).Result;
+            var songfindUrl = songsfind.Response.Hits.FirstOrDefault();
+            if (songfindUrl != null)
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load(songfindUrl.Result.Url);
+                var headerNames = doc.DocumentNode.SelectNodes("//*[@id='lyrics-root']/div[2]");
+                track.Text = System.Web.HttpUtility.HtmlDecode(headerNames.FirstOrDefault().InnerText);
+                if (track.Text == null)
+                    return NotFound();
+            };
 
-			return View(track);
-		}
+            return View(track);
+        }
 
-		[Route("/index-lettres", Name = "IndexLetters")]
-		public IActionResult IndexLetters()
-		{
-			var rappeurs = DataService.Artists.OrderByDescending(a => a.Popularity).Take(100).ToList();
+        [Route("/index-lettres", Name = "IndexLetters")]
+        public IActionResult IndexLetters()
+        {
+            var rappeurs = DataService.Artists.OrderByDescending(a => a.Popularity).Take(100).ToList();
 
             var model = new IndexLetterViewModel()
             {
@@ -129,37 +130,37 @@ namespace app.Controllers
             };
 
             return View(model);
-		}
+        }
 
-		[Route("/lettre_{letter}", Name = "IndexLetter")]
-		public IActionResult IndexLetter(string letter)
-		{
-			if (!Regex.IsMatch(letter, @"^([a-z]|0-9)$"))
-				return NotFound();
+        [Route("/lettre_{letter}", Name = "IndexLetter")]
+        public IActionResult IndexLetter(string letter)
+        {
+            if (!Regex.IsMatch(letter, @"^([a-z]|0-9)$"))
+                return NotFound();
 
-			var rappeurs = new List<SuperArtists>();
+            var rappeurs = new List<SuperArtists>();
 
-			if (letter != "0-9")
-				rappeurs = DataService.Artists.Where(a => Helper.RemoveDiacritics(a.Name).StartsWith(letter)).OrderByDescending(a => a.Popularity).Take(100).ToList();
-			else
-				rappeurs = DataService.Artists.Where(a => Regex.IsMatch(Helper.RemoveDiacritics(a.Name), @"^(?![a-zA-Z])\w+$")).OrderByDescending(a => a.Popularity).Take(100).ToList();
+            if (letter != "0-9")
+                rappeurs = DataService.Artists.Where(a => Helper.RemoveDiacritics(a.Name).StartsWith(letter)).OrderByDescending(a => a.Popularity).Take(100).ToList();
+            else
+                rappeurs = DataService.Artists.Where(a => Regex.IsMatch(Helper.RemoveDiacritics(a.Name), @"^(?![a-zA-Z])\w+$")).OrderByDescending(a => a.Popularity).Take(100).ToList();
 
-			var model = new IndexLetterViewModel()
-			{
-				Artists = rappeurs,
-				PaginationLetters = DataService.GetPaginationLetters()
-			};
+            var model = new IndexLetterViewModel()
+            {
+                Artists = rappeurs,
+                PaginationLetters = DataService.GetPaginationLetters()
+            };
 
-			model.PaginationLetters.FirstOrDefault(l => l.Slug == letter).IsActive = true;
+            model.PaginationLetters.FirstOrDefault(l => l.Slug == letter).IsActive = true;
 
-			return View(model);
-		}
+            return View(model);
+        }
 
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
